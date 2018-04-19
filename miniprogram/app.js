924d15fadcfe08150877fcdf2f79fec8086c2d09
lewis <>
2018-04-19 15:22:33 +0800

    tc

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,5 +1,9 @@
 //app.js
+var qcloud = require('./vendor/wafer2-client-sdk/index')
+var config = require('./config')
+
 App({
-  onLaunch: function () {
-  }
+    onLaunch: function () {
+        qcloud.setLoginUrl(config.service.loginUrl)
+    }
 })
